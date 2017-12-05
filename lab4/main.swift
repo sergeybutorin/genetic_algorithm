8dd20f5c7f7d58e756ed20a4add2d8da0c05e6f8
serbut <serbut555@gmail.com>
2017-12-05 13:58:54 +0300

    Initial Commit

---
+++ lab4/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  lab4
+//
+//  Created by Sergey Butorin on 05/12/2017.
+//  Copyright © 2017 Sergey Butorin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
